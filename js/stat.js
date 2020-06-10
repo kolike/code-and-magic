04bb612d9350e3d01757cec6cf7a407dd2419319
Nikolay <kolik_2604@mail.ru>
2020-06-10 21:56:20 +0300

    Рефакторинг

--- js/stat.js
+++ js/stat.js
@@ -12,7 +12,7 @@ var CLOUD_SHADOW_X = 110;
 var CLOUD_SHADOW_Y = 20;
 var ACTIVE_PLAYER = "Вы";
 
-var renderCloud = function (ctx, x, y, color) {
+var drawCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
@@ -33,25 +33,34 @@ var drawName = function (ctx, players, x, y) {
   ctx.fillText(players, x, y);
 };
 
-var drawBar = function (ctx, x, y, width, height) {
+var drawBar = function (ctx, x, y, width, height, player) {
+  if (player == ACTIVE_PLAYER) {
+    ctx.fillStyle = "rgba(255, 0, 0, 1)";
+  } else {
+    ctx.fillStyle = `rgba(0, 0, 255, ${Math.ceil(Math.random() * 10) / 10})`;
+  }
+
   ctx.fillRect(x, y, width, height);
 };
 
 var drawPoints = function (ctx, points, x, y) {
+  ctx.fillStyle = "#000";
   ctx.fillText(points, x, y);
 };
 
-window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_SHADOW_X, CLOUD_SHADOW_Y, "rgba(0, 0, 0, 0.7");
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, "#fff");
-
+var drawTitle = function (ctx, x, y, gap) {
   ctx.fillStyle = "#000";
+  ctx.font = "16px PT Mono";
+  ctx.fillText("Ура, вы победили!", x + gap, y + gap * 3);
+  ctx.fillText("Список результатов: ", x + gap, y + gap * 5);
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  drawCloud(ctx, CLOUD_SHADOW_X, CLOUD_SHADOW_Y, "rgba(0, 0, 0, 0.7");
+  drawCloud(ctx, CLOUD_X, CLOUD_Y, "#fff");
+  drawTitle(ctx, CLOUD_X, CLOUD_Y, GAP);
 
   var maxTime = getMaxElement(times);
-
-  ctx.font = "16px PT Mono";
-  ctx.fillText("Ура, вы победили!", CLOUD_X + GAP, CLOUD_Y + GAP * 3);
-  ctx.fillText("Список результатов: ", CLOUD_X + GAP, CLOUD_Y + GAP * 5);
 
   for (var i = 0; i < players.length; i++) {
     drawName(
@@ -61,19 +70,15 @@ window.renderStatistics = function (ctx, players, times) {
       CLOUD_HEIGHT - GAP
     );
 
-    if (players[i] == ACTIVE_PLAYER) {
-      ctx.fillStyle = "rgba(255, 0, 0, 1)";
-      } else {
-      ctx.fillStyle = `rgba(0, 0, 255, ${Math.ceil(Math.random() * 10) / 10})`;
-    }
     drawBar(
       ctx,
       CLOUD_X + GAP * 3 + (GAP_BETWEEN_BAR + BAR_WIDTH) * i,
       CLOUD_HEIGHT - GAP * 3,
       BAR_WIDTH,
-      -(BAR_HIGHT * times[i]) / maxTime
+      -(BAR_HIGHT * times[i]) / maxTime,
+      players[i]
     );
-    ctx.fillStyle = "#000";
+
     drawPoints(
       ctx,
       Math.round(times[i]),
